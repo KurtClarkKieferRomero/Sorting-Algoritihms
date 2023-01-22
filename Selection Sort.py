468f17eb3d3eba5b4eb2dd8f1acfb95e160526e3
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-01-22 18:49:21 +0800

    Created the swapping part outside the llop

--- Selection Sort.py
+++ Selection Sort.py
@@ -5,7 +5,8 @@ def sort(nums):
             if nums[j] < nums[minpos]:
                 minpos = j
 
-
-
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
 
 nums = [70, 1, 32, 61, 11, 8, 3, 53, 69, 14]
