72a80ad0164cc57cde1b0d239fda1b8a4dce0b20
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-01-24 19:58:29 +0800

    Finished Bubble sort

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,11 +1,12 @@
 def bubble_sort(nums):
     for i in range(len(nums) - 1):
-        if nums[i] > nums[i + 1]:
-            temp = nums[i]
-            nums[i] = nums[i + 1]
-            nums[i + 1] = temp
+        for j in range(len(nums) - 1 - i):
+            if nums[j] > nums[j + 1]:
+                temp = nums[j]
+                nums[j] = nums[j + 1]
+                nums[j + 1] = temp
 
-
+        print(nums)
 
 nums = [70, 1, 32, 61, 11, 8, 3, 53, 69, 14]
 bubble_sort(nums)
