6bc9229ac958a6c18c65551742509c8cc6e9d1d6
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-01-25 01:15:36 +0800

    Created pivot checker if item is greater or smaller

--- Quick Sort.py
+++ Quick Sort.py
@@ -5,5 +5,14 @@ def quick_sort(nums):
     else:
         pivot = nums.pop()
 
+    elements_greater = []
+    elements_lower = []
+
+    for i in nums:
+        if i > pivot:
+            elements_greater.append(nums)
+        else:
+            elements_lower.append(nums)
+
 
 nums = [70, 1, 32, 61, 11, 8, 3, 53, 69, 14]
