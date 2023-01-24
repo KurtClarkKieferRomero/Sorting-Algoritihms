6c0a77f42cb099fe4bc9fbfd7749ab5c3810b1c8
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-01-24 22:38:28 +0800

    Finished Insertion sort

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -2,10 +2,15 @@ def insertion_sort(nums):
     for i in range(1, len(nums)):
         value = nums[i]
         j = i - 1
-        while j >= 0 and (value < nums[i]):
+        while j >= 0 and (value < nums[j]):
+            nums[j + 1] = nums[j]
+            nums[j] = value
+            j = j - 1
+        print(nums)
 
 
 
 
 nums = [70, 1, 32, 61, 11, 8, 3, 53, 69, 14]
 insertion_sort(nums)
+print(nums)
