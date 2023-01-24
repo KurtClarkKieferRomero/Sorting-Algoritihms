8f99340f4d2b32553b9d5d76147e489c8dc48b96
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2023-01-24 19:50:12 +0800

    Created bubble sort swapping technique

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,4 +1,9 @@
-def bubble_sort():
+def bubble_sort(nums):
+    for i in range(len(nums) - 1):
+        if nums[i] > nums[i + 1]:
+            temp = nums[i]
+            nums[i] = nums[i + 1]
+            nums[i + 1] = temp
 
 
 
